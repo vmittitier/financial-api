e4836e1754c0e5ecf936e57f178cfcce5932540c
vini <mitnewzealand@gmail.com>
2019-12-12 13:53:50 -0300

    initial commit

---
+++ src/test/java/com/devmit/financialapi/FinancialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmit.financialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
